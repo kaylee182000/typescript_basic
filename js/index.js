90e4ea6abbf5f73876d63bda51a8d5632381f95d
kaylee182000 <trungkien40414@gmail.com>
2022-09-12 09:04:39 +0700

    update

--- js/index.js
+++ js/index.js
@@ -13,3 +13,44 @@ var ob = null;
 var number1 = 1; //khong nhat thiet khai bao type phia sau
 //referencee type
 var prod1 = { id: 1, name: "phone 1", price: 1000 };
+var sv = {
+    maSinhVien: "1",
+    tenSinhVien: "khai",
+    lopHoc: "12",
+};
+var svkt = {
+    maSinhVien: "1",
+    tenSinhVien: "khai",
+    lopHoc: "12",
+    cmnd: "323311",
+};
+var result = {
+    maSinhVien: "1",
+    tenSinhVien: "khai",
+    lopHoc: "12",
+};
+//any type, unknown type: kieu du lieu dac biet co the nhan tat ca kieu du lieu khac
+var student = 1;
+var ProductClass = /** @class */ (function () {
+    function ProductClass() {
+        this.id = "";
+        this.name = "";
+    }
+    ProductClass.prototype.showInfo = function () {
+        console.log("id", this.id);
+        console.log("name", this.name);
+    };
+    return ProductClass;
+}());
+var prod5 = new ProductClass();
+prod5.id = 1;
+prod5.name = "khai";
+prod5.showInfo();
+//unknow giong any cho nhan tat ca du lieu Tuy nhien khi thuc hien operation thi phai ktra type
+var prod7 = prod5;
+if (prod7 instanceof ProductClass) {
+    prod7.showInfo();
+}
+if (typeof prod7 === "number") {
+    prod7++;
+}
--- ts/index.ts
+++ ts/index.ts
@@ -18,8 +18,78 @@ let number1 = 1; //khong nhat thiet khai bao type phia sau
 //referencee type
 let prod1: Product = { id: 1, name: "phone 1", price: 1000 };
 
+//type: tao ra mot kieu du lieu cho bien
 type Product = {
   id: number;
   price: number;
   name: string;
+  //desc ?: string optional property
 };
+
+//interface: dung de dinh dang kieu du lieu cho bien tuong tu type nhungw nhieeu tinh nang hon
+
+interface SinhVien {
+  maSinhVien: string;
+  tenSinhVien: string;
+  lopHoc: string;
+}
+
+//Merge: cac interface trung ten thi tu dong merge lai khi bien dich sang js
+interface SinhVien {
+  tuoi?: number;
+}
+
+//Ke thua(extend) : Interface co kha nang ke thua tu interface khac
+interface SinhVienKeToan extends SinhVien {
+  cmnd: string;
+}
+
+let sv: SinhVien = {
+  maSinhVien: "1",
+  tenSinhVien: "khai",
+  lopHoc: "12",
+};
+
+let svkt: SinhVienKeToan = {
+  maSinhVien: "1",
+  tenSinhVien: "khai",
+  lopHoc: "12",
+  cmnd: "323311",
+};
+
+//Gia su server tra ve  1 trong cac kieu du lieu : SinhVien | unf | null
+//union type
+type ResultType = SinhVien | undefined | null | string;
+let result: ResultType = {
+  maSinhVien: "1",
+  tenSinhVien: "khai",
+  lopHoc: "12",
+};
+
+//any type, unknown type: kieu du lieu dac biet co the nhan tat ca kieu du lieu khac
+let student: any = 1;
+
+class ProductClass {
+  id: number | string = "";
+  name: string = "";
+  constructor() {}
+  showInfo() {
+    console.log("id", this.id);
+    console.log("name", this.name);
+  }
+}
+
+let prod5: ProductClass = new ProductClass();
+prod5.id = 1;
+prod5.name = "khai";
+prod5.showInfo();
+
+//unknow giong any cho nhan tat ca du lieu Tuy nhien khi thuc hien operation thi phai ktra type
+
+let prod7: unknown = prod5;
+if (prod7 instanceof ProductClass) {
+  prod7.showInfo();
+}
+if (typeof prod7 === "number") {
+  prod7++;
+}
